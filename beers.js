2c82b997c5c3aa41ec29ac41dddc6480d33f37eb
Greg Mall <gregmall157@gmail.com>
2020-07-13 15:17:36 -0700

    first commit

---
+++ beers.js
@@ -0,0 +1,9 @@
+const PBR = {
+    id: 'beer',
+    name: 'Pabst',
+    image: 'assets/pbr.png',
+    description: 'A cheap beer with a hipster reputation',
+    category: 'american-lager',
+    price: 2.00,
+    
+};
---
+++ test/render-beer.test.js
@@ -0,0 +1,32 @@
+// IMPORT MODULES under test here:
+// import { example } from '../example.js';
+// import { }
+import { renderBeer } from '../render-beer.js';
+const test = QUnit.test;
+
+QUnit.module('Render Beer');
+
+test('renders beers', expect => {
+   
+    // Set up your arguments and expectations
+    const PBR = {
+        
+        id: 'beer',
+        name: 'Pabst',
+        image: 'assets/pbr.jpg',
+        description: 'A cheap beer with a hipster reputation',
+        category: 'american-lager',
+        price: 2.00
+        
+        
+    };
+    const expected = '<li class="american-lager" title="A cheap beer with a hipster reputation"><h3>Pabst</h3><img src="assets/pbr.jpg" alt="Pabst image"><p class="price">$2.00<button value="beer">Add</button></p></li>';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const dom = renderBeer(PBR);
+    const html = dom.outerHTML;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(html, expected);
+});
