59d5ad629ff9174f3ca07b5b8b4c5dda9786472c
Greg Mall <gregmall157@gmail.com>
2020-07-20 09:07:21 -0700

    fixes

--- admin/admin.js
+++ admin/admin.js
@@ -7,6 +7,7 @@ import { getBeers } from '../utils.js';
 const myForm = document.getElementById('add-form');
 
 myForm.addEventListener('submit', e => {
+  
     e.preventDefault();
     
     const formData = new FormData(myForm);
@@ -19,17 +20,19 @@ myForm.addEventListener('submit', e => {
         image: formData.get('image'),
         description: formData.get('description'),
         category: formData.get('category'),
-        price: formData.get('price')
+        price: formData.get(Number('price'))
       
     };
-    
+     
     const existingBeer = getBeers();
-    existingBeer.push(newBeer); 
-console.log(newBeer);
-    const stringBeers = JSON.stringify(existingBeer);
-    localStorage.setItem('BEER', stringBeers);
     
-    window.location = '../products/index.html';
+    existingBeer.push(newBeer); 
+    console.log(existingBeer);
+
+    const stringBeers = JSON.stringify(existingBeer);
+    localStorage.setItem('BEERS', stringBeers);
+    
+    window.location = '../products/products.html';
     
  
 });  
